607b52db6562a8446c9ccfe3d5f95e9e62f3f0cb
Alexandra Galdina <nerenisa@gmail.com>
2019-08-12 17:03:23 +0300

    format

--- ucac4_test_folder/ucac4all.py
+++ ucac4_test_folder/ucac4all.py
@@ -9,17 +9,17 @@ import struct
 import os
 
 
-z_catalog = добавить   # folder with directories
-binary_unpack = добавить  # format characters module struct (78 bytes)
-col = [добавить]
-pg_engine = create_engine('postgresql+psycopg2://user@localhost:5433/test2')
-psql = 'select * from "ucac4" limit 300;'
+z_catalog = r'C:\Users\User\Documents\UCAC4---UCAC5\ucac4_test_folder'     # folder with directories
+binary_unpack = 'iihhbbbBBbbbhhhhBB'  # format characters module struct (78 bytes)
+col = ['ra', 'spd', 'momag', 'apmag', 'sigmag', 'objt', 'dsf', 'sigra', 'sigdc', 'nt', 'ns', 'nc', 'cepra', 'cepdc', 'pmrac', 'pmdc', 'sigpmr', 'sigpmd', 'pts_key', 'jm', 'hm', 'km', 'fj', 'fh', 'fks', 'ejm', 'ehm', 'ekm', 'bma', 'vma', 'gma', 'rma', 'ima', 'ebma', 'evma', 'egma', 'erma', 'eima', 'gcflg', 'tsf', 'acmf', 'bcmf', 'hcmf', 'zcmf', 'bbcmf', 'lcmf', 'ncmf', 'scmf', 'leda', 'x2m', 'rnm', 'zn2', 'rn2']
+#pg_engine = create_engine('postgresql+psycopg2://user@localhost:5433/test2')
+#psql = 'select * from "ucac4" limit 300;'
 
 
 counter_w=0
 
 # walk through files in a directory z_catalog
-zfiles = [f for f in os.listdir(z_catalog) if f.startswith("z")]
+zfiles = [f for f in os.listdir(z_catalog) if f.startswith("z001")]
 for z_files in zfiles:
     full_name_path = os.path.join(z_catalog, z_files)
     with open(full_name_path, 'rb') as fin:
@@ -28,13 +28,13 @@ for z_files in zfiles:
         all_catalog = []   # creating a temporary list of decoded binary directory strings z326....z900
         s = []    # creation of a temporary list from binary directory line numbering z326....z900
         while din:
-            din = fin.read(78)   # 78 bytes one row
-            if len(din) == 78:
+            din = fin.read(30)   # 78 bytes one row
+            if len(din) == 30:
                 list_row = list(struct.unpack(binary_unpack, din)) 
-                #print(list_row)
-                all_catalog.append(list_row)
-                n = n + 1
-                s.append(str(n).zfill(6))
+                print(list_row)
+                #all_catalog.append(list_row)
+                #n = n + 1
+                #s.append(str(n).zfill(6))
         #zn = ''.join((z_files.lstrip('z'), '-')).split() * all_catalog.__len__() 
         #idn = [zn[i] + s[i] for i in range(len(s))]  
         #df = pd.DataFrame(all_catalog, index = idn, columns = col)
