4eb7fc4a2c577fdf17c75e7929e213bc93f090c0
Alexandra <nerenisa@gmail.com>
2019-08-15 22:22:01 +0300

    reload py

--- ucac4_test_folder/ucac4all.py
+++ ucac4_test_folder/ucac4all.py
@@ -8,6 +8,7 @@ import numpy as np
 import struct
 import os
 
+
 # items to read from input files 
        # I | unsigned int   | integer 4 | ra, spd, pts_key, tsf, rnm, rn
        # H | unsigned short | integer 2 | momag, apmag, cepra, cepdc, gma, rma, ima, zn2
@@ -19,8 +20,8 @@ import os
 z_catalog = r'R:\Git_hub\UCAC4---UCAC5\ucac4_test_folder'       #'/home/source_cat/UCAC4/u4b'     #r'C:\Users\User\Documents\UCAC4---UCAC5\ucac4_test_folder'       # folder with directories
 binary_unpack = '=IIHHBBBBBBBBHHhhBBIhhhBBBBBBhhHHHbbbbbBIBBIHI'             # format characters module struct (78 bytes)
 col = ['ra', 'spd', 'momag', 'apmag', 'sigmag', 'objt', 'dsf', 'sigra', 'sigdc', 'nt', 'ns', 'nc', 'cepra', 'cepdc', 'pmrac', 'pmdc', 'sigpmr', 'sigpmd', 'pts_key', 'jm', 'hm', 'km', 'fj', 'fh', 'fks', 'ejm', 'ehm', 'ekm', 'bma', 'vma', 'gma', 'rma', 'ima', 'ebma', 'evma', 'egma', 'erma', 'eima', 'gcflg', 'tsf', 'leda', 'x2m', 'rnm', 'zn2', 'rn2']
-pg_engine = create_engine('postgresql+psycopg2://user@localhost:5433/test2')
-psql = 'select * from "ucac4" limit 300;'
+#pg_engine = create_engine('postgresql+psycopg2://user@localhost:5433/test2')
+#psql = 'select * from "ucac4" limit 300;'
 
 
 counter_w=0
@@ -38,14 +39,14 @@ for z_files in zfiles:
             din = fin.read(78)   # 78 bytes one row
             if len(din) == 78:
                 list_row = list(struct.unpack(binary_unpack, din)) 
-                print(list_row)
+                #print(list_row)
                 all_catalog.append(list_row)
                 #n = n + 1
                 #s.append(str(n).zfill(6))
         #zn = ''.join((z_files.lstrip('z'), '-')).split() * all_catalog.__len__() 
         #idn = [zn[i] + s[i] for i in range(len(s))]  
-        df = pd.DataFrame(all_catalog, columns = col)
-        print(df)
+        #df = pd.DataFrame(all_catalog, columns = col)
+        #print(df)
         #if counter_w == 0:
             #df.to_sql('ucac4', index=False, con=pg_engine)
             #counter_w = 1
