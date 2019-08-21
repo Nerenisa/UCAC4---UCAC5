473c5addb1e2b21ee473692c1aab079ea83623bf
Alexandra Galdina <nerenisa@gmail.com>
2019-08-21 16:43:28 +0300

    update encodezfile.py

--- encodebinfile/encodezfile.py
+++ encodebinfile/encodezfile.py
@@ -9,26 +9,22 @@ import struct
 import os
 import sys
 
+#files = []
+binary_pack = '=IIHHBBBBBBBBHHhhBBIhhhBBBBBBhhHHHbbbbbBIBBIHI'
+pg_engine = create_engine('postgresql+psycopg2://user@localhost:5433/test2')
+s = list(range(0, 648000000, 720000))       # 
+#print(s)
+d = list(range(720000, 648720000, 720000))
+#print(d)
+for i, x in zip(s, d):
+    psql = ' '.join(('select * from "ucac4" where spd >=', str(i), 'and', 'spd <', str(x), 'order by ra;'))
+    pg_df = pd.read_sql_query(psql, con=pg_engine)
+    #print(pg_df)
+    write_list = []
+    for row in pg_df.itertuples(index=False, name=False):
+        binary = struct.pack(binary_pack, *row)
+        print(binary)
+        #write_list.append(binary)
 
-
-
-
-
-
-
-
-
-
-
-#binary_pack = ''
-#pg_engine = create_engine('postgresql+psycopg2://user@localhost:5433/test2')
-#psql = 'select * from "urat1.3";'
-
-
-#pg_df = pd.read_sql_query(psql, con=pg_engine)
-#print(pg_df)
-#for row in pg_df.itertuples(index=False, name=False):
-    #binary = struct.pack(binary_pack, *row)
-    #print(binary)
     #with open('binaryf', 'ab') as fout:
           #fout.write(binary)
