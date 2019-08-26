addc9b1efeabbfb4f5094e417d4f247669fa69f9
user <user@dbtest2.sai.msu.ru>
2019-08-26 19:18:08 +0300

    add comment

--- encodebinfile/encodezfile.py
+++ encodebinfile/encodezfile.py
@@ -12,16 +12,16 @@ import sys
 
 binary_pack = '=IIHHBBBBBBBBHHhhBBIhhhBBBBBBhhHHHbbbbbBIBBIHI'
 pg_engine = create_engine('postgresql+psycopg2://user@localhost:5433/test2')
-s = list(range(0, 648000000, 720000))       # 
-d = list(range(720000, 648720000, 720000))
+s = list(range(0, 648000000, 720000))       # wide 0.2 deg = 720000  (from 0 to 180 deg, step 0.2)
+d = list(range(720000, 648720000, 720000))  
 z = 0
 for i, x in zip(s, d):
     files = []
-    psql = ' '.join(('select * from "ucac4" where spd >=', str(i), 'and', 'spd <', str(x), 'order by ra;'))
+    psql = ' '.join(('select * from "ucac4" where spd >=', str(i), 'and', 'spd <', str(x), 'order by ra;'))  # database query 
     pg_df = pd.read_sql_query(psql, con=pg_engine)
     #print(pg_df)
     z = z + 1
-    files.append(''.join(('z', str(z).zfill(3))))
+    files.append(''.join(('z', str(z).zfill(3))))   
     for row in pg_df.itertuples(index=False, name=False):
         #print(row)
         binary = struct.pack(binary_pack, *row)
